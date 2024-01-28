d87b25e7e552bfb449af24292083be7697e87341
Jeremy E <34139712+JeremyEastham@users.noreply.github.com>
2024-01-28 10:32:41 -0600

    10:32 yikes

--- custom_tools.py
+++ custom_tools.py
@@ -1,14 +1,30 @@
+import random
+from datetime import datetime
 from typing import Optional
 
 from google.oauth2.credentials import Credentials
 from llama_hub.tools.gmail import GmailToolSpec
 from llama_hub.tools.google_calendar import GoogleCalendarToolSpec
+from llama_index.tools import FunctionTool
 from llama_index.tools.tool_spec.base import BaseToolSpec
 
 
 class CustomGmailToolSpec( GmailToolSpec ):
+    # spec_functions = [
+    #     "load_data",
+    #     "search_messages",
+    #     "create_draft",
+    #     "update_draft",
+    #     "get_draft",
+    #     "send_draft",
+    # ]
     def __init__( self, user_file = None ):
         self.user_file = user_file
+        self.spec_functions.remove( "load_data" )
+
+    def search_messages( self, query: str, max_results: Optional[ int ] = None ):
+        """Searches the user's Gmail account using the specified query string."""
+        return super().search_messages( query, max_results )
 
     def _get_credentials( self ):
         if self.user_file is None:
@@ -18,8 +34,11 @@ class CustomGmailToolSpec( GmailToolSpec ):
         )
 
 class CustomGoogleCalendarToolSpec( GoogleCalendarToolSpec ):
+    # spec_functions = [ "load_data", "create_event", "get_date" ]
+
     def __init__( self, user_file = None ):
         self.user_file = user_file
+        self.spec_functions.remove( "load_data" )
 
     def _get_credentials( self ):
         if self.user_file is None:
@@ -27,3 +46,35 @@ class CustomGoogleCalendarToolSpec( GoogleCalendarToolSpec ):
         return Credentials.from_authorized_user_info( self.user_file,
      [ "https://www.googleapis.com/auth/calendar" ]
         )
+
+transaction_history = [
+    { "date": "1/27/24", "amount": "$5.64", "location": "McDonald's", "category": "fast food" },
+    { "date": "1/26/24", "amount": "$32.46", "location": "Walmart", "category": "groceries" },
+    { "date": "1/25/24", "amount": "$9.74", "location": "Chik-fil-A", "category": "fast food" },
+    { "date": "1/25/24", "amount": "$16.98", "location": "Target", "category": "groceries" },
+    { "date": "1/23/24", "amount": "$4.53", "location": "Starbucks", "category": "fast food" },
+    { "date": "1/22/24", "amount": "$127.37", "location": "Best Buy", "category": "technology" },
+]
+
+weather = [ { "temp": "72", "condition": "sunny" }, { "temp": "48", "condition": "snowy" }, { "temp": "80", "condition": "raining" } ]
+
+def make_tool( fn, name, description ) -> FunctionTool:
+    return FunctionTool.from_defaults( fn = fn, name = name, description = description )
+
+
+date_tool = make_tool( lambda: datetime.now().strftime( "%d/%m/%Y" ), "date",
+                       "Returns the current date in the format DD/MM/YYYY" )
+time_tool = make_tool( lambda: datetime.now().strftime( "%H:%M" ), "time",
+                       "Returns the current time in the format HH:MM" )
+bank_tool = make_tool( lambda: transaction_history,
+                       "bank", "Returns the user's transaction history" )
+remember_tool = make_tool( lambda text = None: f"Okay, I saved this note for you: {text}", "remember",
+                           "Saves a note on the user's device" )
+reminder_tool = make_tool( lambda when = None, description = None: f"Okay, I set a reminder for {when}: {description}",
+                           "set_reminder", "Sets an alarm on the user's device" )
+play_music_tool = make_tool( lambda song = None: "Now streaming to Spotify.", "play_music",
+                             "Searches for a song on Spotify and plays it on the user's device." )
+set_setting_tool = make_tool( lambda setting_id = None, value = None: f"Set {setting_id} to {value}", "set_setting",
+                              "Modifies a setting on the user's device." )
+get_location_tool = make_tool( lambda: "College Station, TX", "get_location", "Returns the user's current location." )
+get_weather_tool = make_tool( lambda location = None: random.choice( weather ), "get_weather", "Returns the user's current weather." )
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from llama_hub.tools.gmail import GmailToolSpec
 from llama_hub.tools.google_calendar import GoogleCalendarToolSpec
 from llama_hub.tools.slack import SlackToolSpec
 from llama_hub.tools.wikipedia import WikipediaToolSpec
-from llama_index import download_loader, set_global_service_context, ServiceContext, PromptHelper, VectorStoreIndex
+from llama_index import download_loader, set_global_service_context, ServiceContext, PromptHelper, VectorStoreIndex, \
+    SummaryIndex
 from llama_index.agent import OpenAIAgent, ReActAgent, OpenAIAgentWorker, AgentRunner
 import openai
 from llama_index.agent.types import TaskStepOutput
@@ -18,9 +19,11 @@ from llama_index.node_parser import TextSplitter, SentenceSplitter
 from llama_index.tools.tool_spec.load_and_search import LoadAndSearchToolSpec
 from starlette.responses import StreamingResponse
 
-from custom_tools import CustomGmailToolSpec, CustomGoogleCalendarToolSpec
+from custom_tools import CustomGmailToolSpec, CustomGoogleCalendarToolSpec, date_tool, time_tool, bank_tool, \
+    reminder_tool, remember_tool, play_music_tool, set_setting_tool, get_location_tool, get_weather_tool
 from get_token_json import authorize_google
 
+dumb = True
 
 def load_api_keys():
     global api_keys
@@ -49,10 +52,26 @@ authorize_google()
 gmail = CustomGmailToolSpec()
 calendar = CustomGoogleCalendarToolSpec()
 tools = [ gmail, calendar ]
-tools = [ tool for tool_list in tools for tool in tool_list.to_tool_list() ]
-# tools = [ LoadAndSearchToolSpec.from_defaults( tool_list, index_cls = VectorStoreIndex ).to_tool_list() for tool_spec in tools for tool_list in
+# tools = [ tool for tool_list in tools for tool in tool_list.to_tool_list() ]
+# tools = [ LoadAndSearchToolSpec.from_defaults( tool_list ).to_tool_list() for tool_spec in tools for tool_list in
 #           tool_spec.to_tool_list() ]
-# tools = [ tool for sublist in tools for tool in sublist ]
+tools = [ tool for sublist in tools for tool in sublist.to_tool_list() ]
+las_tools = [ "search_messages" ]
+skip_tools = [ "create_event" ]
+new_tools = []
+for tool in tools:
+    name = tool.metadata.name
+    if name in las_tools:
+        load, search = LoadAndSearchToolSpec.from_defaults( tool, index_cls = SummaryIndex ).to_tool_list()
+        # search.metadata.name = name
+        search.metadata.description = f"Once data has been loaded from {load.metadata.name}, the result is stored in a vector database. " \
+                                       "This database can be queried using this function. " \
+                                       "The natural language query indicates what text should be retrieved."
+        new_tools += [ load, search ]
+    elif name not in skip_tools:
+        new_tools.append( tool )
+tools = new_tools
+tools += [ date_tool, time_tool, bank_tool, reminder_tool, remember_tool, play_music_tool, set_setting_tool, get_location_tool, get_weather_tool ]
 
 app = FastAPI()
 
@@ -67,30 +86,30 @@ async def chat( message: str = Body(), google_user_file: str = Query(), chat_his
         parsed_chat_history.append( ChatMessage( role = MessageRole.USER if user else MessageRole.ASSISTANT, content = entry ) )
         user = not user
 
-    # agent = ReActAgent.from_tools(
-    #     tools,
-    #     llm = OpenAI(
-    #         model = "gpt-3.5-turbo",
-    #         system_prompt = "Act as a helpful assistant. You have extensive access to a variety of APIs to securely access personal information and more!"
-    #     ),
-    #     verbose = True
-    # )
-    worker = OpenAIAgentWorker.from_tools(
+    agent = ReActAgent.from_tools(
         tools,
         llm = OpenAI(
-            model = "gpt-3.5-turbo",
-            system_prompt = "Act as a helpful assistant. You have extensive access to a variety of APIs to securely access personal information and more!",
-            chat_history = parsed_chat_history
+            model = "gpt-3.5-turbo" if dumb else "gpt-4-0125-preview",
+            system_prompt = "Act as a helpful assistant. You have extensive access to a variety of APIs to securely access personal information and more!"
         ),
         verbose = True
     )
-    agent = AgentRunner( worker )
-    task = agent.create_task( message )
-    output: TaskStepOutput | None = None
-    while output is None or not output.is_last:
-        output = agent.run_step( task.task_id )
-    response = agent.finalize_response( task.task_id, output )
-    # response = agent.chat( message, parsed_chat_history ).response
+    # worker = OpenAIAgentWorker.from_tools(
+    #     tools,
+    #     llm = OpenAI(
+    #         model = "gpt-3.5-turbo",
+    #         system_prompt = "Act as a helpful assistant. You have extensive access to a variety of APIs to securely access personal information and more!",
+    #         chat_history = parsed_chat_history
+    #     ),
+    #     verbose = True
+    # )
+    # agent = AgentRunner( worker )
+    # task = agent.create_task( message )
+    # output: TaskStepOutput | None = None
+    # while output is None or not output.is_last:
+    #     output = agent.run_step( task.task_id )
+    # response = agent.finalize_response( task.task_id, output ).response
+    response = agent.chat( message, parsed_chat_history ).response
     print( f"Human: {message}\nAssistant: {response}" )
     return response
 
